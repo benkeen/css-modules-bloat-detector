0e09bf9cc6401974635225e85f50c795482fa72a
Ben Keen <Ben.Keen@globalrelay.net>
2019-10-09 16:19:00 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App () {
+    return (
+        <div>
+            <h1>CSS Modules Bloat Detector</h1>
+            <section>
+                code here
+            </section>
+            <section>
+                generated here
+            </section>
+        </div>
+    );
+}
+
+export default App;
